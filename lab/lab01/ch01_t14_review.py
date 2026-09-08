62b9741635b4cad9bd9c5cb714429b40afff3c8b
NgPakLam-2627 <ngpaklam103@gmail.com>
2026-09-08 04:41:24 +0000

    [skip actions] lab/lab01/ch01_t14_review.py

--- lab/lab01/ch01_t14_review.py
+++ lab/lab01/ch01_t14_review.py
@@ -2,4 +2,4 @@ skill_completed = "Python Syntax"
 exercises_completed = 13
 point_per_exercise = 5
 point_total = 100
-total += exercises_completed * point_per_exercise
+point_total += exercises_completed * point_per_exercise
