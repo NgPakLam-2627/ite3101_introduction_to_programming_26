df54d1826e4ff0b08fcc1529afb2c0ca42888cdb
NgPakLam-2627 <ngpaklam103@gmail.com>
2026-09-08 03:26:00 +0000

    [skip actions] lab/lab01/ch01_t13_value_error.py

--- lab/lab01/ch01_t13_value_error.py
+++ lab/lab01/ch01_t13_value_error.py
@@ -1,4 +1,4 @@
 float_1 = 0.25
 float_2 = 40.0
-string_addition = float1 + float2 
-int_addition = int(float1) + int(float2) 
+string_addition = float_1 + float_2
+int_addition = int(float_1) + int(float_2)
