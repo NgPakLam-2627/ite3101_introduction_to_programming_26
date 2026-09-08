9ee99b4e7720e767a32e71a66c2213a60ba23661
NgPakLam-2627 <ngpaklam103@gmail.com>
2026-09-08 02:38:13 +0000

    [skip actions] lab/lab01/ch01_t06_arithmetic.py

--- lab/lab01/ch01_t06_arithmetic.py
+++ lab/lab01/ch01_t06_arithmetic.py
@@ -6,7 +6,7 @@ sassy_combinations = 129 * 1345 + 120 / 6 - 12
 is_this_number_odd = 15 % 2 
 is_this_number_divisible_by_seven = 133 % 7
 
-print("")
+print("mirthful_addition")
 print("")
 print("")
 print("")
