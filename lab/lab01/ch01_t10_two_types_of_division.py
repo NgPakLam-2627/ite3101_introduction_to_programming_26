3d7af64290d7d515de88798cec1fea4f9f00cd33
NgPakLam-2627 <ngpaklam103@gmail.com>
2026-09-08 03:22:17 +0000

    [skip actions] lab/lab01/ch01_t10_two_types_of_division.py

--- lab/lab01/ch01_t10_two_types_of_division.py
+++ lab/lab01/ch01_t10_two_types_of_division.py
@@ -2,4 +2,4 @@ cucumbers = 100+
 num_people = 6
 whole_cucumbers_per_person = cucumbersm // num_people
 print(whole_cucumbers_per_person)
-float_cucumbers_per_person = 
+float_cucumbers_per_person = cucumbersm / num_people
