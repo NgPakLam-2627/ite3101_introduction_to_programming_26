a32a0b7dd0e47ed4da910ed5816c0529c328adef
NgPakLam-2627 <ngpaklam103@gmail.com>
2026-09-08 02:34:30 +0000

    [skip actions] lab/lab01/ch01_t06_arithmetic.py

--- lab/lab01/ch01_t06_arithmetic.py
+++ lab/lab01/ch01_t06_arithmetic.py
@@ -3,3 +3,5 @@ amazing_subtraction = 981 - 312
 trippy_multiplication = 38 * 902 
 happy_division = 540 / 45 
 sassy_combinations = 129 * 1345 + 120 / 6 - 12
+is_this_number_odd = 15 % 2 
+is_this_number_divisible_by_seven = 133 % 7
