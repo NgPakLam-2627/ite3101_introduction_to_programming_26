fb0f4295b349af0d20ad8831b5caed734e94f506
NgPakLam-2627 <ngpaklam103@gmail.com>
2026-09-22 02:08:23 +0000

    lab/lab04/ch04_t02_compare_closely.py

--- lab/lab04/ch04_t02_compare_closely.py
+++ lab/lab04/ch04_t02_compare_closely.py
@@ -7,10 +7,10 @@ bool_one = True  # We did this one for you!
 bool_two = True
 
 # Set this to True if 19 <= 19 or to False if it is not.
-bool_three = None
+bool_three = True
 
 # Set this to True if -22 >= -18 or to False if it is not.
-bool_four = None
+bool_four = False
 
 # Set this to True if 99 != (98 + 1) or to False otherwise.
-bool_five = None
+bool_five = False
